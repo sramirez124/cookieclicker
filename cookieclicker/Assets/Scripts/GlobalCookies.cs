86db08f86396c6bf536ea7dfc1a0e3c988110aac
Steven-Ramirez <sramriez124@gmail.com>
2020-03-22 19:30:13 -0500

    Added Scirpts and Buttons to Make/Sell Cookies

---
+++ cookieclicker/Assets/Scripts/GlobalCookies.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalCookies : MonoBehaviour
+{
+
+    public static int cookieCount;
+    public GameObject cookieDisplay;
+    public int internalCookie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        internalCookie = cookieCount;
+        cookieDisplay.GetComponent<Text>().text = "Cookies: " + internalCookie;
+    }
+}
