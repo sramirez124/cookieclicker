d420d8a3bb9bbd5e113ff7d7d739e6bc3824c907
Steven-Ramirez <sramriez124@gmail.com>
2020-05-11 14:34:45 -0500

    fixed scripts

--- cookieclicker/Assets/Scripts/SceneManager.cs
+++ cookieclicker/Assets/Scripts/SceneManager.cs
@@ -10,7 +10,7 @@ public class SceneManager : MonoBehaviour
 
     // GlobalCookies variables
     [Header("Cookie Settings")]
-    [SerializeField] private int cookieCount;
+    [SerializeField] private float cookieCount;
     [SerializeField] private GameObject cookieDisplay;
 
     // GlobalUpgrades variables
@@ -18,24 +18,24 @@ public class SceneManager : MonoBehaviour
     [SerializeField] private Button bakerButton;
     [SerializeField] private GameObject bakerText;
     [SerializeField] private GameObject bakerStats;
-    [SerializeField] private int numOfBakers;
-    [SerializeField] private int bakerAutoPerSec;
+    [SerializeField] private float numOfBakers;
+    [SerializeField] private float bakerAutoPerSec;
 
     // GlobalCash variables
     [Header("Money Settings")]
-    [SerializeField] private static int cashCount;
+    [SerializeField] private static float cashCount;
     [SerializeField] private GameObject CashDisplay;
     [SerializeField] private GameObject textBox;
     [SerializeField] private GameObject statusBox;
 
     [Header("AutoCookie Settings")]
     public bool creatingCookie;
-    public int cookieIncrease = 1;
-    public int internalIncrease; // figure out what this does
+    public float cookieIncrease = 1;
+    public float internalIncrease;
 
     // Cost for inital upgrade variables
     [Header("Inital Upgrade Cost")]
-    [SerializeField] private int bakerValue = 1;
+    [SerializeField] private float bakerValue = 50;
     // Start is called before the first frame update
     void Start()
     {
@@ -54,7 +54,7 @@ public class SceneManager : MonoBehaviour
       // Upgrades Display Updates
       bakerStats.GetComponent<Text>().text = "Bakers: " + numOfBakers + " @ " + bakerAutoPerSec +" Per Second";
       bakerText.GetComponent<Text>().text = "Hire New Baker - $" + bakerValue;
-      TurnOn();
+      TurnOn(bakerValue, bakerButton);
 
 
     }
@@ -79,42 +79,43 @@ public class SceneManager : MonoBehaviour
 
     }
 
-    public void TurnOn() // used to activate the baker upgrade. Needs to be made to work with out
+    public void TurnOn(float upgradeValue, Button upgradeButton)
     {
-
-      if (cashCount >= bakerValue)
+      if (cashCount >= upgradeValue)
       {
-        bakerButton.interactable = true;
+        upgradeButton.interactable = true;
       }
       else
       {
-        bakerButton.interactable = false;
+        upgradeButton.interactable = false;
       }
     }
 
-    public void AutoCookie() // I have no clue what internalIncrease does but it works for now
+    public void StartAutoCookie(float upgradeValue, Button upgradeButton, float upgradePerSec) // this is what starts the auto cookie process
+      {
+        cashCount -= upgradeValue;
+        upgradeValue *= 2;
+        upgradeButton.interactable = false;
+        upgradePerSec += 0.5F;
+        numOfBakers += 1;
+        AutoCookie(upgradePerSec);
+      }
+
+
+    public void AutoCookie(float upgradePerSec)
     {
-        cookieIncrease = bakerAutoPerSec;
+        cookieIncrease = upgradePerSec;
         internalIncrease = cookieIncrease;
         creatingCookie = true;
         StartCoroutine(CreateTheCookie());
     }
 
-    public void StartAutoCookie()
-    {
-      cashCount -= bakerValue;
-      bakerValue *= 2;
-      bakerButton.interactable = false;
-      bakerAutoPerSec += 1;
-      numOfBakers += 1;
-      AutoCookie();
-    }
 
     IEnumerator CreateTheCookie()
     {
       cookieCount += internalIncrease;
       yield return new WaitForSeconds(1);
       creatingCookie = false;
-      AutoCookie();
+      AutoCookie(bakerAutoPerSec);
     }
 }
